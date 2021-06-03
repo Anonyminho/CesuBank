d335cd3af33755901776a7a977d87a2932b69484
Anonyminho <67487494+Anonyminho@users.noreply.github.com>
2021-06-03 18:24:20 -0300

    LandingPage Done

---
+++ public/javascript/index.js
@@ -0,0 +1,13 @@
+const links = 
+[
+    "www.cesupa.br",
+    "../public/landing-page.html",
+    "../public/funcionalidades.html",
+    "../public/criadores.html", 
+    "../public/cadastro.html",
+    "../public/login.html"
+]
+
+function redirect(index) {
+    window.location = links[index]
+}
