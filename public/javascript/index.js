6a7e1ee73073ced20024d26aded4bf7832b3041e
Newton Miranda <newton.chickengopro@gmail.com>
2021-06-05 16:43:55 -0300

    Página -> Minha Conta

--- public/javascript/index.js
+++ public/javascript/index.js
@@ -1,13 +1,14 @@
-const links = 
-[
-    "https://www.cesupa.br/",
-    "../public/landing-page.html",
-    "../public/funcionalidades.html",
-    "../public/criadores.html", 
-    "../public/cadastro.html",
-    "../public/login.html"
-]
+const links = [
+  'https://www.cesupa.br/',
+  '../public/landing-page.html',
+  '../public/funcionalidades.html',
+  '../public/criadores.html',
+  '../public/cadastro.html',
+  '../public/login.html',
+];
 
+// eslint-disable-next-line no-unused-vars
 function redirect(index) {
-    window.location.href = links[index]
+  // eslint-disable-next-line no-undef
+  window.location.href = links[index];
 }
